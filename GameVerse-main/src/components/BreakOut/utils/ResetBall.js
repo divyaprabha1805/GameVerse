c901d5ce094050548496b3c07cbed969d9da2624
Divya Prabha BB <138508191+divyaprabha1805@users.noreply.github.com>
2025-04-10 13:35:14 +0530

    Delete GameVerse-main directory

--- GameVerse-main/src/components/BreakOut/utils/ResetBall.js
+++
@@ -1,6 +0,0 @@
-export default function ResetBall(ball, canvas, racquet) {
-    ball.x = racquet.x;
-    ball.y = racquet.y - 80;
-    ball.dx = 6;
-    ball.dy = -6;
-}
--- GameVerse-main/src/components/Tetris/business/Cell.js
+++
@@ -1,5 +0,0 @@
-export const defaultCell = {
-    occupied: false,
-    className: ""
-  };
-  
